bb010436dba4cb5eee0bb077774597d50725336b
Sanjay krishna Manavarthi <89273866+Msks1218@users.noreply.github.com>
2022-02-13 19:57:06 -0600

    Add files via upload

---
+++ manavarthi_calculator/manavarthi_calculator/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  manavarthi_calculator
+//
+//  Created by Manavarthi,Sanjay Krishna on 2/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func allclear(_ sender: UIButton) {
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+    }
+    @IBAction func plusorminus(_ sender: UIButton) {
+    }
+    @IBAction func dividebutton(_ sender: UIButton) {
+    }
+    @IBAction func numberseven(_ sender: UIButton) {
+    }
+    @IBAction func numbereight(_ sender: UIButton) {
+    }
+    
+    @IBAction func numbernine(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiplybutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func numberfour(_ sender: UIButton) {
+    }
+    
+    @IBAction func numberfive(_ sender: UIButton) {
+    }
+    
+    @IBAction func numbersix(_ sender: UIButton) {
+    }
+    
+    @IBAction func minusbutton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func numberone(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func numbertwo(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func numberthree(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func plusbutton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func numberzero(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func dotbutton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func percentagebutton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func equalsbutton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
